d874cee4c55ea4c8f553e9382f8a080187d80bf1
MSchwinner <marsch1992@gmail.com>
2020-08-20 23:39:20 +0200

    forecasting

---
+++ R/functions.R
@@ -0,0 +1,17 @@
+import_csv_data <- function(path = NULL) {
+  
+  # import csvs
+  filenames = list.files(path = path, pattern="*.csv")
+  
+  filepath <- paste0(path, filenames)
+  
+  data <- lapply(filepath, read.csv) #import data in list
+  
+  #renaming
+  df_names <- str_extract(filenames, '.*(?=\\.csv)')
+  
+  names(data) <- df_names
+  
+  return(data)
+  
+}
